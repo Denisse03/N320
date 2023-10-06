049dde466cb1bcc43f914ce8d1ef892b6e3072fb
Denisse03 <99741771+Denisse03@users.noreply.github.com>
2023-10-05 22:53:57 -0400

    hw4

--- app/array.js
+++ app/array.js
@@ -70,5 +70,180 @@ console.log("Has Even Number: ", hasEvenNum([1, 2, 3, 4, 5]));
 console.log("Has Even Number: ", hasEvenNum([21, 23, 33, 43, 15]));
 
 //filter
+var fArr = [1, 2, 3];
+
+var firstFilter = fArr.filter(function (value) {
+  return value > 2;
+});
+
+console.log("First Filter: ", firstFilter);
+
+//only names with four letters
+var names = ["Carrie", "Gabe", "Ben", "Abbey", "Will", "Bredon"];
+
+function fourLetters(arr) {
+  return arr.filter(function (value) {
+    return value.length === 4;
+  });
+}
+
+console.log("Four Letters: ", fourLetters(names));
+
+var classList = ["N125", "N220", "N320", "N317", "N423", "N215"];
+
+function upperLevel(arr) {
+  return arr.filter(function (value) {
+    return value.substr(1, 3) >= 300;
+  });
+}
+
+console.log("Class Filter: ", upperLevel(classList));
+
+let students = [
+  { first: "Greg", last: "Smith", employed: true },
+  { first: "Sally", last: "Richards" },
+  { first: "Jake", last: "Farmer", employed: true },
+  { first: "Kelce", last: "Roger" },
+];
+
+function filterByIndex(arr, key) {
+  return arr.filter(function (value) {
+    return !value[key];
+  });
+}
+
+console.log("Not Employed: ", filterByIndex(students, "employed"));
+
+let students2 = [
+  { first: "Greg", last: "Smith", employed: true },
+  { first: "Sally", last: "Richards", employed: false },
+  { first: "Jake", last: "Farmer", employed: true },
+  { first: "Kelce", last: "Roger", employed: false },
+];
+
+console.log("Not Employed 2: ", filterByIndex(students, "employed"));
 
 //Map
+let mArr = [2, 3, 4, 5, 6];
+
+let mapArray = mArr.map(function (value, index) {
+  return { key: index, value: value * value };
+});
+
+console.log("Squared: ", mapArray);
+
+//First names only
+function firstNames(arr) {
+  return arr.map(function (value) {
+    return value.first;
+  });
+}
+
+console.log("Only First Names: ", firstNames(students2));
+
+//Value * Index
+function vtIndex(arr) {
+  return arr.map(function (value, index) {
+    return value * index;
+  });
+}
+
+console.log("Value Times Index: ", vtIndex([2, 5, 9]));
+
+//Reduce
+//it has 2 part which is a callback function and initialValue
+//callback: accumulator, nextValue
+
+let reduceArr = [1, 2, 3, 4, 5];
+
+var reduceFn = reduceArr.reduce(function (accumulator, nextValue) {
+  return accumulator + nextValue;
+}, 10);
+
+console.log("Reduce fn 1: ", reduceFn);
+
+var reduceFn2 = reduceArr.reduce(function (accumulator, nextValue) {
+  return accumulator + nextValue;
+});
+
+console.log("Reduce fn 2: ", reduceFn2);
+
+let family = ["William", "Abigail", "Richie", "Ben", "Gabe", "Mike"];
+//Output: My family members are William Abigail Richie
+
+let nameFunction = family.reduce(function (accumulator, nextValue) {
+  return (accumulator += " " + nextValue);
+}, "My family is");
+
+console.log("Name Reduction: ", nameFunction);
+
+//Object Reduction
+var familyMembers = [
+  { first: "Abigial", age: 24 },
+  { first: "William", age: 21 },
+  { first: "Richie", age: 11 },
+];
+function extractValue(arr, key) {
+  return arr.reduce(function (accumulator, nextValue) {
+    accumulator.push(nextValue[key]);
+    return accumulator;
+  }, []);
+}
+
+//Short/Long family Names
+//Using family array
+
+function nameLength(value) {
+  return value.length > 4;
+}
+
+function partition(arr, callback) {
+  return arr.reduce(
+    function (accumulator, nextValue) {
+      if (callback(nextValue)) {
+        accumulator[0].push(nextValue);
+      } else {
+        accumulator[1].push(nextValue);
+      }
+      return accumulator;
+    },
+    [[], []]
+  );
+}
+
+console.log(partition(family, nameLength));
+
+console.log("First Name Array ", extractValue(familyMembers, "first"));
+
+//Code Challenge #1
+function hasAZ(str) {
+  return str.split("").some(function (value) {
+    return value == "z";
+  });
+}
+console.log("Has a Z", hasAZ("The quick brown fox jumps over the lazy hound."));
+
+//Code Challenge #2
+function hasOnlyOddNumbers(arr) {
+  return arr.every(function (value) {
+    return value % 2 !== 0;
+  });
+}
+
+console.log("Only odd: " + hasOnlyOddNumbers([3, 4, 5, 7, 8, 9]));
+console.log("Only odd 2: " + hasOnlyOddNumbers([3, 3, 7, 9, 11, 9]));
+
+//Code Challenge #3
+
+var arrs = [1, 2, 3, 4, 5];
+
+var reduceFn = arr.reduce(function (accumulator, nextValue) {
+  let num = nextValue * nextValue;
+  console.log(num);
+  if (num % 2 == 0) {
+    return accumulator + num;
+  }
+  return accumulator;
+}, 50);
+
+console.log("Reduce Function", reduceFn);
---
+++ homework_four/arraysAssignment.js
@@ -0,0 +1,75 @@
+const students = [
+  { name: "Alice", grades: [85, 92, 78, 90] },
+
+  { name: "Bob", grades: [75, 88, 95, 100] },
+
+  { name: "Charlie", grades: [60, 72, 68, 74] },
+
+  { name: "David", grades: [92, 88, 95, 98] },
+
+  { name: "Eve", grades: [100, 100, 100, 100] },
+];
+
+//foreach
+const studentNames = [];
+
+students.forEach((student) => {
+  studentNames.push(student.name);
+});
+
+document.getElementsByClassName("sList")[0].innerHTML += studentNames;
+
+//every method
+
+const gradeCheck = students.every(function (student) {
+  return student.grades.every(function (grade) {
+    return grade >= 60;
+  });
+});
+
+document.getElementsByTagName("p")[1].innerHTML += gradeCheck;
+
+//some method
+
+const someCheck = students.some(function (student) {
+  return student.grades.some(function (grade) {
+    return (grade = 100);
+  });
+});
+
+document.getElementsByTagName("p")[2].innerHTML += someCheck;
+
+//filter
+//run two methods
+// 1. Go through each student
+// 2.For each student:
+//   - add up all the grades and divide by the total number of grades
+// - Take that answer, and see if it’s greater than 90
+
+const averageStudent = students.filter(function (student) {
+  const averageGrade =
+    student.grades.reduce((avg, grade) => avg + grade) / student.grades.length;
+
+  return averageGrade >= 90;
+});
+
+const studentName = averageStudent.map(function (student) {
+  return student.name;
+});
+document.getElementsByClassName("sList")[1].innerHTML += studentName;
+
+//map
+const studentSummaries = students.map((student) => {
+  const averageGrade =
+    student.grades.reduce((total, grade) => total + grade, 0) /
+    student.grades.length;
+  return `Name: ${student.name}, Average Grade: ${averageGrade}`;
+});
+
+document.getElementById("studentAvgs").innerHTML = studentSummaries;
+//reduce
+const studentGrades = students.reduce((total, student) => {
+  return total + student.grades.length;
+}, 0);
+
+document.getElementsByTagName("p")[5].innerHTML += studentGrades;
